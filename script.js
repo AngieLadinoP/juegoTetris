53a93bac908fbed4daad60113749d075e6a88089
Angie <agladinop@gmail.com>
2021-12-13 11:58:53 -0500

    comentarios y boton re-start

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const canvas = document.getElementById("canva")
-const ctx = canvas.getContext("2d")
-const scoreElement = document.getElementById("score")
+let canvas = document.getElementById("canva")
+let ctx = canvas.getContext('2d')
 
 //Number of columns and rows the board will have 
 const COLS = 10;
 const ROWS = 20;
-
 // Block size: it is a square, width and height are going to be the same 
 const BLOCK_SIZE = 20;
-
 //Color of squares that are not taken by any figure
 const EMPTY = "#EFEFEF";
+//Variable of Score
+const scoreElement = document.getElementById("score");
 
-
-//Draw the squares of the board
+//Draw the sq of the board
 function drawSq(x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
@@ -41,8 +39,7 @@ function drawBoard() {
 }
 drawBoard();
 
-
-//Define the pieces and the positions they have 
+//Define the pieces and the positions they have
 const I = [
     [
         [0, 0, 0, 0],
@@ -162,7 +159,7 @@ const Z = [
     ]
 ];
 
-//Array with previous pieces and their colors
+//Array with pieces and their colors
 const PIECES = [
     [Z, "red"],
     [S, "green"],
@@ -173,30 +170,28 @@ const PIECES = [
     [J, "orange"]
 ];
 
-//Generate random pieces.   Choose a random number that will be the index to select the piece from the array 
+//Generate random pieces. Choose a random number that will be the index to select the piece from the array
 function randomPiece() {
     let ranPiece = Math.floor(Math.random() * PIECES.length) //Index position to select figure from array of pieces
     return new Piece(PIECES[ranPiece][0], PIECES[ranPiece][1]) // selected figure and its color as parameters of the object: Piece 
 }
 
 let p = randomPiece();
-//Object piece 
 
+//The object piece
 function Piece(figure, color) {
     this.figure = figure;
     this.color = color;
 
     //We start from the first position of the figure 
     this.figureN = 0; // Index for the array of positions of each piece
-
     this.actFigure = this.figure[this.figureN]; // selected figure in a specific position
 
     //Define the location of the pieces when they appear on the board for the firs time
     this.x = 3; // the image appears in the center
     this.y = -2; //The image is over the board and starts falling
 }
-
-/* In order to move the figure we need to:
+/* In order to move the figure (left, right, down) we need to:
  - Evaluate future position of the figure to know if threre are any collisions
  - Undraw the current figure
  - Translate the figure's coordinates to the desire place
@@ -205,10 +200,11 @@ function Piece(figure, color) {
 
 
 
-// Draw the figure
+//fill function
 Piece.prototype.fill = function(color) {
     for (r = 0; r < this.actFigure.length; r++) { // Array's rows  of the figure's specific position 
         for (c = 0; c < this.actFigure.length; c++) { // Array's columns  of the figure's specific position 
+            //We draw only the occupied sq
             if (this.actFigure[r][c]) { // each square in the array of the specific position of the figure 
                 drawSq(this.x + c, this.y + r, color);
             }
@@ -216,30 +212,29 @@ Piece.prototype.fill = function(color) {
     }
 }
 
-//Draw the figure
+//Draw a piece to the board
 Piece.prototype.draw = function() {
     this.fill(this.color);
 }
 
-//Undraw the figure
+//Undraw a piece
 Piece.prototype.unDraw = function() {
     this.fill(EMPTY);
 }
 
 //Move down the piece
 Piece.prototype.moveDown = function() {
-    if (!this.collision(0, 1, this.actFigure)) {
-        this.unDraw();
-        this.y++;
-        this.draw();
-    } else {
-        //We look the piece and generate a new one
-        this.lock();
-        p = randomPiece();
+        if (!this.collision(0, 1, this.actFigure)) {
+            this.unDraw();
+            this.y++;
+            this.draw();
+        } else {
+            //We look the piece and generate a new one
+            this.lock();
+            p = randomPiece();
+        }
     }
-}
-
-//Move right the piece
+    //Move right the piece
 Piece.prototype.moveRight = function() {
         if (!this.collision(1, 0, this.actFigure)) {
             this.unDraw();
@@ -249,27 +244,25 @@ Piece.prototype.moveRight = function() {
     }
     //Move left the piece
 Piece.prototype.moveLeft = function() {
-    if (!this.collision(-1, 0, this.actFigure)) {
-        this.unDraw();
-        this.x--;
-        this.draw();
+        if (!this.collision(-1, 0, this.actFigure)) {
+            this.unDraw();
+            this.x--;
+            this.draw();
+        }
     }
-}
-
-//Rotate the piece
+    //Rotate the piece
 Piece.prototype.rotate = function() {
     let nextPat = this.figure[(this.figureN + 1) % this.figure.length];
     let kick = 0;
 
     if (this.collision(0, 0, nextPat)) {
-        if (this.x > COLS / 2) {
-            // it's the right wall
-            kick = -1; // we need to move the piece to the left
+        if (this.x < COLS / 2) {
+            kick = -1; //We need to move the piece to the left
         } else {
-            // it's the left wall
-            kick = 1; // we need to move the piece to the right
+            kick = 1; //We need to move the piece to the right
         }
     }
+
     if (!this.collision(kick, 0, nextPat)) {
         this.unDraw();
         this.x += kick;
@@ -278,3 +271,126 @@ Piece.prototype.rotate = function() {
         this.draw();
     }
 }
+
+let score = 0;
+
+Piece.prototype.lock = function() {
+    for (r = 0; r < this.actFigure.length; r++) {
+        for (c = 0; c < this.actFigure.length; c++) {
+            //We skip the empty sq
+            if (!this.actFigure[r][c]) {
+                continue;
+            }
+            // pieces to lock on top = game over
+            if (this.y + r < 0) {
+                alert("Game Over");
+                //Stop request animation frame
+                gameOver = true;
+                break;
+            }
+
+            //We lock the piece
+            board[this.y + r][this.x + c] = this.color;
+        }
+    }
+
+    //Remove full rows
+    for (r = 0; r < ROWS; r++) {
+        let isRowFull = true;
+        for (c = 0; c < COLS; c++) {
+            isRowFull = isRowFull && (board[r][c] != EMPTY);
+        }
+        if (isRowFull) {
+            // if the row is full
+            // we move down all the rows above it
+            for (y = r; y > 1; y--) {
+                for (c = 0; c < COLS; c++) {
+                    board[y][c] = board[y - 1][c];
+                }
+            }
+            // the top row board[0][..] has no row above it
+            for (c = 0; c < COLS; c++) {
+                board[0][c] = EMPTY;
+            }
+            // increment the score
+            score += 10;
+        }
+    }
+    // update the board
+    drawBoard();
+
+    // update the score
+    scoreElement.innerHTML = score;
+}
+
+// collision function
+Piece.prototype.collision = function(x, y, piece) {
+    for (r = 0; r < piece.length; r++) {
+        for (c = 0; c < piece.length; c++) {
+            // if the square is empty, we skip it
+            if (!piece[r][c]) {
+                continue;
+            }
+            // coordinates of the piece after movement
+            let newX = this.x + c + x;
+            let newY = this.y + r + y;
+
+            // conditions
+            if (newX < 0 || newX >= COLS || newY >= ROWS) {
+                return true;
+            }
+            // skip newY < 0; board [-1] will crush our game
+            if (newY < 0) {
+                continue;
+            }
+            // check if there is a locked piece alrady in place
+            if (board[newY][newX] != EMPTY) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Control the piece
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event) {
+    if (event.key == "ArrowLeft") {
+        p.moveLeft();
+        dropStart = Date.now();
+    } else if (event.key == "ArrowUp") {
+        p.rotate();
+        dropStart = Date.now();
+    } else if (event.key == "ArrowRight") {
+        p.moveRight();
+        dropStart = Date.now();
+    } else if (event.key == "ArrowDown") {
+        p.moveDown();
+    }
+}
+
+//Drop the piece every 1sec
+let dropStart = Date.now();
+let gameOver = false;
+
+function drop() {
+    let now = Date.now();
+    let delta = now - dropStart;
+    if (delta > 500) {
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    if (!gameOver) {
+        requestAnimationFrame(drop);
+    }
+}
+drop();
+
+//This function start all
+/*function start(){
+    canvas = document.getElementById("canva")
+    ctx = canvas.getContext('2d')
+    drawBoard();
+	drop();
+}*/
